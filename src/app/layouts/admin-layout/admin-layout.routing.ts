a731fb2885b9feb66f352a573b29088661cdf832
David Rodríguez Coto <drodriguezc1@ucenfotec.ac.cr>
2024-03-03 18:01:07 -0600

    Added logos and removed trash. Also edited coloration of various sections

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -2,17 +2,16 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { IconsComponent } from '../../pages/icons/icons.component';
-import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
 import {UsersComponent} from "../../pages/users/users.component";
 import {InventoryComponent} from "../../pages/inventory/inventory.component";
+import {InventoryLogComponent} from "../../pages/inventory-log/inventory-log.component";
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
     { path: 'users',      component: UsersComponent },
     { path: 'inventory',      component: InventoryComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'tables',         component: TablesComponent },
+    { path: 'inventory-log',      component: InventoryLogComponent },
     { path: 'icons',          component: IconsComponent }
 ];
---
+++ src/app/pages/inventory-log/inventory-log.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tables',
+  templateUrl: './inventory-log.component.html',
+  styleUrls: ['./inventory-log.component.scss']
+})
+export class InventoryLogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
